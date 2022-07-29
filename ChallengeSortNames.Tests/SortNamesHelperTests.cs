c7e3cf21ca0cb1b73988b869a1a5b639e83debc3
Ivan Dario Villota <idvillota@gmail.com>
2022-07-29 12:46:02 -0500

    Implement solution for SortNames Challenge

---
+++ ChallengeSortNames.Tests/SortNamesHelperTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Xunit;
+
+namespace ChallengeSortNames.Tests
+{
+    public class SortNamesHelperTests
+    {
+        [Fact]
+        public void SortNames_CorrectParameters_SuccessfullyOrderedNames()
+        {
+            var names = new string[] { "Sonia", "Maria", "Wood", "Dempster" };
+            var newOrder = new int[] { 4, 1, 2, 3 };
+            var orderedNames = new string[] { "Dempster", "Sonia", "Maria", "Wood" };
+
+            var result = SortNamesHelper.SortNames(names, newOrder);
+
+            Assert.Equal(orderedNames.Length, result.Length);
+            Assert.Equal(orderedNames[0], result[0]);
+        }
+
+        [Fact]
+        public void SortNames_SendFourNamesAndThreeOrderPositions_ThrowsArgumentException()
+        {
+            var names = new string[] { "Sonia", "Maria", "Wood", "Dempster" };
+            var newOrder = new int[] { 4, 1,  };
+
+            var exception = Assert.Throws<ArgumentException>(() => SortNamesHelper.SortNames(names, newOrder));
+
+            Assert.Equal("The number of names is different from the number of positions to order", exception.Message);
+        }
+
+        [Fact]
+        public void SortNames_SendDuplicatedOrderPositions_ThrowsArgumentException()
+        {
+            var names = new string[] { "Sonia", "Maria", "Wood", "Dempster" };
+            var newOrder = new int[] { 4, 1, 4, 2 };
+
+            var exception = Assert.Throws<ArgumentException>(() => SortNamesHelper.SortNames(names, newOrder));
+
+            Assert.Equal("There are duplicate elements in the order array", exception.Message);
+        }
+
+        [Fact]
+        public void SortNames_SendIndexLowerToInitialPosition_ThrowsArgumentException()
+        {
+            var names = new string[] { "Sonia", "Maria", "Wood", "Dempster" };
+            var newOrder = new int[] { 0, 1, 2, 3 };
+
+            var exception = Assert.Throws<ArgumentException>(() => SortNamesHelper.SortNames(names, newOrder));
+
+            Assert.Equal("The new indexes are out of array length", exception.Message);
+        }
+
+        [Fact]
+        public void SortNames_SendIndexHigherThanNumberOfNames_ThrowsArgumentException()
+        {
+            var names = new string[] { "Sonia", "Maria", "Wood", "Dempster" };
+            var newOrder = new int[] { 5, 1, 2, 3 };
+
+            var exception = Assert.Throws<ArgumentException>(() => SortNamesHelper.SortNames(names, newOrder));
+
+            Assert.Equal("The new indexes are out of array length", exception.Message);
+        }
+
+        [Fact]
+        public void SortNames_SendNullInNamesParameter_ThrowsArgumentException()
+        {
+            string[] names = null;
+            var newOrder = new int[] { 5, 1, 2, 3 };
+
+            var exception = Assert.Throws<ArgumentException>(() => SortNamesHelper.SortNames(names, newOrder));
+
+            Assert.Equal("The names and/or new order are null", exception.Message);
+        }
+
+        [Fact]
+        public void SortNames_SendNullInNewOrderParameter_ThrowsArgumentException()
+        {
+            var names = new string[] { "Sonia", "Maria", "Wood", "Dempster" };
+            int[] newOrder = null;
+
+            var exception = Assert.Throws<ArgumentException>(() => SortNamesHelper.SortNames(names, newOrder));
+
+            Assert.Equal("The names and/or new order are null", exception.Message);
+        }
+
+        [Fact]
+        public void SortNames_SendNullInNamesParameterAndNullInNewOrderParameter_ThrowsArgumentException()
+        {
+            string[] names = null;
+            int[] newOrder = null;
+
+            var exception = Assert.Throws<ArgumentException>(() => SortNamesHelper.SortNames(names, newOrder));
+
+            Assert.Equal("The names and/or new order are null", exception.Message);
+        }
+    }
+}
+
---
+++ ChallengeSortNames/SortNamesHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace ChallengeSortNames
+{
+	public static class SortNamesHelper
+	{
+        public static string[] SortNames(string[] names, int[] newOrder)
+        {
+            if (names == null || newOrder == null)
+                throw new ArgumentException("The names and/or new order are null");
+
+            if (names.Length != newOrder.Length)
+                throw new ArgumentException("The number of names is different from the number of positions to order");
+
+            if (newOrder.Length != newOrder.Distinct().Count())
+                throw new ArgumentException("There are duplicate elements in the order array");
+
+            var maxIndex = newOrder.Max();
+            var minIndex = newOrder.Min();
+
+            if (maxIndex > names.Length || minIndex < 1)
+                throw new ArgumentException("The new indexes are out of array length");
+
+            var orderedNames = new string[names.Length];
+
+            for (int i = 0; i < newOrder.Length; i++)
+            {
+                var newIndex = newOrder[i] - 1;
+                orderedNames[i] = names[newIndex];
+            }
+
+            return orderedNames;
+        }
+    }
+}
+
